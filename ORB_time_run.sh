6bd55a4fd85f065a2c2d0b3455d82653cfbb3c56
chenchang(陈常) <1253419722@qq.com>
2019-05-09 16:24:18 +0800

    Add files via upload

---
+++ ORB_time_run.sh
@@ -0,0 +1,65 @@
+echo "MH01"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH01.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_MH01.txt
+
+echo "MH02"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav2/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav2/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH02.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_MH02.txt
+
+echo "MH03"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav3/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav3/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH03.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_MH03.txt
+
+echo "MH04"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav4/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav4/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH04.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_MH04.txt
+
+echo "MH05"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav5/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/mav5/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH05.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_MH05.txt
+
+echo "V101"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_01_easy/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_01_easy/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V101.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V101.txt
+
+echo "V102"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_02_medium/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_02_medium/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V102.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V102.txt
+
+echo "V103"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_03_difficult/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V1_03_difficult/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V103.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V103.txt
+
+echo "V201"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_01_easy/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_01_easy/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V201.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V201.txt
+
+echo "V202"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_02_medium/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_02_medium/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V202.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V202.txt
+
+echo "V203"
+cd ~/code/ORB_SLAM2
+./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_03_difficult/mav0/cam0/data  /media/cc/LENOVO_USB_HDD/data/EuRoC/V2_03_difficult/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/V203.txt
+cd ~/output
+mv ORB_perimage_process_time.txt  ORB_perimage_process_time_V203.txt
